377f02885ac3615f51a7ba1eca5d44bf3ddac1b7
Reuben Chay <>
2022-09-11 22:18:40 +0800

    Fixed error when launching fxml window

--- src/main/java/Duke/MainWindow.java
+++ src/main/java/Duke/MainWindow.java
@@ -23,7 +23,7 @@ public class MainWindow extends AnchorPane {
     private Duke duke;
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/pickleRick.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/meeseeks.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/meseeks.png"));
 
     @FXML
     public void initialize() {
